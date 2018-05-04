551c8c4e625fe1bda2ff5157b55b82b64579b294
iamnikhilupadhyay <iamnikhilupadhyay1014@gmail.com>
2018-05-05 00:53:22 +0530

    form validate part 2

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,3 +1,8 @@
 myApp.controller('RegistrationController',['$scope',function($scope){
   $scope.message="Welcome to my App";
-}])
+
+
+  $scope.register = function(){
+    $scope.message="Welcome "+$scope.user.firstname+" "+$scope.user.lastname+" !!";
+  };
+}]);
