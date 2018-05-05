beb750bdf61edd65a395e6f4dae64b2f31dc32f4
iamnikhilupadhyay <iamnikhilupadhyay1014@gmail.com>
2018-05-06 02:31:25 +0530

    node and express setup

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,8 +1,19 @@
-myApp.controller('RegistrationController',['$scope',function($scope){
-  $scope.message="Welcome to my App";
+myApp.controller('RegistrationController',['$scope','$firebase','$firebaseAuth',function($scope,$firebase,$firebaseAuth){
+  
+  var ref = $firebase.database().ref();
+  var auth = $firebaseAuth();
 
+  $scope.login = function(){
+    $scope.message="Welcome "+$scope.user.email;
+  };
 
   $scope.register = function(){
-    $scope.message="Welcome "+$scope.user.firstname+" "+$scope.user.lastname+" !!";
+
+    auth.$createUserWithEmailAndPassword(
+      $scope.user.email,
+      $scope.user.password
+    ).then(function(regUser){
+      $scope.message="Hi "+$scope.user.firstname+" "+$scope.user.lastname+", Thanks for registering!!";
+    });
   };
 }]);
